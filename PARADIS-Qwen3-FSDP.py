d4c560fed2c443f418c62015f3c1eaa8d9286bf7
tqhb2502 <tqhb2502@gmail.com>
2025-06-21 03:22:12 +0700

    fsdp-4bit-quantization

--- PARADIS-Qwen3-FSDP.py
+++ PARADIS-Qwen3-FSDP.py
@@ -248,7 +248,12 @@ def load_model_n_tokenizer(config, device):
         tokenizer.pad_token = tokenizer.eos_token
 
     # Cấu hình 4-bit quantization
-    quantization_config = BitsAndBytesConfig(load_in_8bit=True)
+    quantization_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_compute_dtype=torch.float16,
+        bnb_4bit_use_double_quant=True,
+        bnb_4bit_quant_type="nf4",
+    )
 
     # Load model
     model = AutoModelForCausalLM.from_pretrained(
