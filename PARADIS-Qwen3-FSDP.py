d712937b5b3a8f17a01ca82dfba1a8fbadcea5dd
tqhb2502 <tqhb2502@gmail.com>
2025-06-22 13:42:49 +0700

    fsdp-switch-to-1.7b-2

--- PARADIS-Qwen3-FSDP.py
+++ PARADIS-Qwen3-FSDP.py
@@ -614,7 +614,7 @@ def fsdp_wrap(model):
         model,
         auto_wrap_policy=transformer_auto_wrapper_policy,
         mixed_precision=fp16_policy,
-        # cpu_offload=CPUOffload(offload_params=True),
+        cpu_offload=CPUOffload(offload_params=False),
         backward_prefetch=None,
         sharding_strategy=ShardingStrategy.FULL_SHARD,
     )
